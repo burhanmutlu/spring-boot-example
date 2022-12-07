c8666c2b9ce401ffa17b86ab4e340efcc906a489
burhanmutlu <burhackmutlu@gmail.com>
2022-12-07 22:36:13 +0300

    son hali

--- src/main/java/kodlama/io/northwind/dataAccess/abstracts/ProductDao.java
+++
@@ -1,9 +0,0 @@
-package kodlama.io.northwind.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kodlama.io.northwind.entities.concretes.Product;
-
-public interface ProductDao extends JpaRepository<Product, Integer> {
-	
-}
